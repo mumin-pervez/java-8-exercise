d92d95f76e0639f0b6ef0f5db6ab819f7962cf5b
mumin-pervez <pervez.ist@gmail.com>
2025-08-23 20:10:50 +0600

    Add exception handling method

---
+++ src/exceptionhandling/ExceptionHandlingDemo.java
@@ -0,0 +1,13 @@
+package exceptionhandling;
+
+public class ExceptionHandlingDemo {
+
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 0;
+    System.out.println("a = "+a+" and b = "+b);
+    int result = a / b;
+    System.out.println(result);
+  }
+
+}
